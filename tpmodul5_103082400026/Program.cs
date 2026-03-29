ed479eed85a36c6e17be3a88edd426916d685dbb
Ridho Ramadhan <ridho.ramadan1016@gmail.com>
2026-03-30 04:42:49 +0700

    menambahkan class HalpGeneric dengan method menyapaUser

--- tpmodul5_103082400026/Program.cs
+++ tpmodul5_103082400026/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace tpmodul5_103082400026
+{
+    public class HaloGeneric
+    {
+        public void SapaUser<T>(T x)
+        {
+            Console.WriteLine("Halo user " + x);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HaloGeneric halo = new HaloGeneric();
+
+            halo.SapaUser<String>("Ridho Ramadhan");
+
+            Console.WriteLine("\nTekan Enter untuk keluar...");
+            Console.ReadLine();
+        }
+    }
+}
