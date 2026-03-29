6c76336558899376653428059a1f5548018b41f6
Ridho Ramadhan <ridho.ramadan1016@gmail.com>
2026-03-30 04:49:12 +0700

    menambahkan class DataGeneric

--- tpmodul5_103082400026/Program.cs
+++ tpmodul5_103082400026/Program.cs
@@ -1,1 +1,33 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace tpmodul5_103082400026
+{
+    public class DataGeneric<T>
+    {
+        public T Data { get; set; }
+
+        public DataGeneric(T data)
+        {
+            this.Data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Data yang tersimpan adalah: " + this.Data);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+   
+            DataGeneric<String> dataNIM = new DataGeneric<String>("103082400026");
+
+            dataNIM.PrintData();
+
+            Console.WriteLine("\nTekan Enter untuk keluar...");
+            Console.ReadLine();
+        }
+    }
+}
